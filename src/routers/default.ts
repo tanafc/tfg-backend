02b096492a5bee13c0cfbc40284a692d1a26418f
Tanausu <tanausu@acidtango.com>
2023-04-17 01:40:46 +0100

    Refactor: Clean imports

--- src/routers/default.ts
+++ src/routers/default.ts
@@ -1,10 +1,7 @@
-import * as express from 'express';
+import * as express from "express";
 
-/**
- * Contains all non-specified routes
- */
 export const defaultRouter = express.Router();
 
-defaultRouter.all('*', (_, res) => {
+defaultRouter.all("*", (_, res) => {
   res.status(501).send();
 });
